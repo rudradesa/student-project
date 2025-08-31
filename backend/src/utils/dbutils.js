122384f393db1cac420059b355a059399296bace
rudradesa <130157151+rudradesa@users.noreply.github.com>
2025-08-31 12:28:30 +0530

    Add files via upload

---
+++ backend/src/utils/dbutils.js
@@ -0,0 +1,33 @@
+import { pool } from "../config/db.js";
+
+const userTableQuery = `CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL, -- Add password field; using VARCHAR(255) for hashed passwords
+    userType ENUM('user','admin') DEFAULT 'user',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);`;
+
+
+
+const createTable = async (tableName, query) => {
+  try {
+    await pool.query(query);
+    console.log(`${tableName} table created or already exists`);
+  } catch (error) {
+    console.log(`Error creating ${tableName}`, error);
+  }
+};
+
+const createAllTable = async () => {
+  try {
+    await createTable("Users", userTableQuery);
+    console.log("All tables created successfully!!");
+  } catch (error) {
+    console.log("Error creating tables", error);
+    throw error;
+  }
+};
+
+export default createAllTable;
